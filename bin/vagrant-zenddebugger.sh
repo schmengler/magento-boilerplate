60625327e7baf0671f215a646bfe0e40bf734a6d
Fabian Schmengler <fs@integer-net.de>
2014-09-02 08:59:35 +0200

    Fixed loading of zend key

--- bin/vagrant-zenddebugger.sh
+++ bin/vagrant-zenddebugger.sh
@@ -2,7 +2,7 @@
 # based on http://forums.zend.com/viewtopic.php?f=59&t=115943
 if [ ! -f /vagrant/var/ZendDebugger.so ]; then
 	cd /tmp
-	wget -Q "http://repos-source.zend.com/zend-server/6.3/deb/pool/zend-server-php-5.5-common_6.3.0+b31_amd64.deb"
+	wget "http://repos-source.zend.com/zend-server/6.3/deb/pool/zend-server-php-5.5-common_6.3.0+b31_amd64.deb"
 	ar x zend-server-php-5.5-common_6.3.0+b31_amd64.deb
 	tar -zxf data.tar.gz
 	#--> usr/local/zend/lib/debugger/php-5.5.x/ZendDebugger.so
