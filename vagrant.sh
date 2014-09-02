03fda25b950c2db7dc39e31f43a8a5b17cf52743
Fabian Schmengler <fs@integer-net.de>
2014-09-02 08:51:50 +0200

    Improve vagrant.sh startup script: always show rsync-auto output, allow restarting, do not kill everything.

--- vagrant.sh
+++ vagrant.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
-echo Starting Vagrantbox...
-vagrant up
-echo Rsync shared folders...
-vagrant rsync-auto > var/log/rsync.log 2> var/log/rsync_error.log &
-echo "Now running rsync in the background (pid=$!)."
-echo Output is logged to var/log/rsync.log
-
+start_rsync_auto() {
+	echo Rsync shared folders...
+	vagrant rsync-auto &
+	echo "Now running rsync in the background (pid=$!)."
+}
 kill_rsync_auto() {
     echo "Kill rsync-auto processes..."
     kill -INT $!
-    # also kill windows processes, workaround from https://github.com/mitchellh/vagrant/issues/3824
-    WMIC path win32_process get Processid,Commandline | grep rsync-auto | awk '{print $NF}' | xargs -n 1 taskkill /f /pid
 }
 
+echo Starting Vagrantbox...
+vagrant up
+start_rsync_auto;
+
 while true; do
-    read -p "Press H to halt the virtual machine, S to suspend, L to login via SSH: " hs
+    read -p "Press H to halt the virtual machine, S to suspend, L to login via SSH, R to restart rsync-auto: " hs
     case $hs in
         [Hh]* ) kill_rsync_auto; echo "Halt Vagrantbox..."; vagrant halt; break;;
         [Ss]* ) kill_rsync_auto; echo "Suspend Vagrantbox..."; vagrant suspend; break;;
         [Ll]* ) echo "Connecting to Vagrantbox..."; vagrant ssh; continue;;
+		[Rr]* ) kill_rsync_auto; echo "Restart rsync-auto..."; start_rsync_auto; continue;
         * ) echo "Not recognized command $hs.";;
     esac
 done
