b3f199c97ebdfba10b5dbdc99a0fa19470a6a68d
Fabian Schmengler <fs@integer-net.de>
2014-09-18 09:39:56 +0200

    Delete default admin user before creating

--- puphpet/files/exec-once/91-magento-config.sh
+++ puphpet/files/exec-once/91-magento-config.sh
@@ -3,4 +3,5 @@ n98-magerun config:set dev/log/active 1
 n98-magerun config:set dev/template/allow_symlink 1
 
 # Admin user
+n98-magerun admin:user:delete admin
 n98-magerun admin:user:create admin admin@example.com test123 Achmed Admin
