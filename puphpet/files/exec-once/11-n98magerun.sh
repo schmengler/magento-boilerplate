7898450130320ecd5e438efb499bafa5d6d1b6f4
Fabian Schmengler <fs@integer-net.de>
2014-11-07 12:04:33 +0100

    Set up autocompletion for n98-magerun

--- puphpet/files/exec-once/11-n98magerun.sh
+++ puphpet/files/exec-once/11-n98magerun.sh
@@ -1,3 +1,5 @@
 # n98-magerun
 curl -s -o /usr/local/bin/n98-magerun https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar
 chmod +x /usr/local/bin/n98-magerun
+
+wget -q -O /etc/bash_completion.d/n98-magerun.phar https://raw.githubusercontent.com/netz98/n98-magerun/master/autocompletion/bash/bash_complete
