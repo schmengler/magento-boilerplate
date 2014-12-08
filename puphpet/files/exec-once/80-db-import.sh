ba0430d4536c6073bd3218217f2c08d63fadd82f
Fabian Schmengler <fs@integer-net.de>
2014-12-08 13:47:55 +0100

    Choose template interactively in first call to "up"

--- puphpet/files/exec-once/80-db-import.sh
+++ puphpet/files/exec-once/80-db-import.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-DUMP=`ls -1t /vagrant/systemstorage/live/database/*.sql.gz | head -n1`
+echo "Looking for database dump in systemstorage/live/database/..."
+DUMP=`ls -1t /vagrant/systemstorage/live/database/*.sql.gz 2> /dev/null | head -n1`
 
-echo "Importing SQL dump from $DUMP. This will take a while..."
-echo "You can ignore warnings about using a password on the command line interface." >&2
+if [ "$DUMP" != "" ]; then
 
-DBH="localhost";
-DBU="root"
-DBP="root"
-DBN="magento"
+	echo "Importing SQL dump from $DUMP. This will take a while..."
+	echo "You can ignore warnings about using a password on the command line interface." >&2
 
-echo "1) Drop database"
-(echo "SET foreign_key_checks = 0;"; 
- (mysqldump -h ${DBH} -u${DBU} -p${DBP} --add-drop-table --no-data ${DBN} | 
-     grep ^DROP); 
- echo "SET foreign_key_checks = 1;") | mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN}
+	DBH="localhost";
+	DBU="root"
+	DBP="root"
+	DBN="magento"
 
-echo "2) Import database"
-gunzip < ${DUMP} | mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN}
+	echo "1) Drop database"
+	(echo "SET foreign_key_checks = 0;"; 
+	 (mysqldump -h ${DBH} -u${DBU} -p${DBP} --add-drop-table --no-data ${DBN} | 
+		 grep ^DROP); 
+	 echo "SET foreign_key_checks = 1;") | mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN}
 
-echo "Done importing SQL dump."
+	echo "2) Import database"
+	gunzip < ${DUMP} | mysql -h ${DBH} -u${DBU} -p${DBP} ${DBN}
+
+	echo "Done importing SQL dump."
+else
+	echo "No database dump found."
+fi
