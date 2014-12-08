a562cb7bf27ea817ed30cb5710823752ef4e7598
Fabian Schmengler <fs@integer-net.de>
2014-12-08 16:26:43 +0100

    Allinone-Template: Install Magento CE 1.9.1.0 if no installation found in source dir

---
+++ puphpet/templates/allinone/exec-once/70-install-magento.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# This script uses n98-magerun to install a fresh Magento in the repository if it is not already there
+#
+DOMAIN=`cat /vagrant/etc/domain`
+MAGENTO_VERSION=magento-ce-1.9.0.1
+INSTALL_DIR=/vagrant/`cat /vagrant/etc/src`
+
+if [ -f "$INSTALL_DIR/app/Mage.php" ]; then
+	n98-magerun install --dbHost="localhost" --dbUser="magento" --dbPass="root" --dbName="magento" --installSampleData=no --useDefaultConfigParams=yes --magentoVersionByName="$MAGENTO_VERSION" --installationFolder="$INSTALL_DIR" --baseUrl="http://$DOMAIN/"
+fi
